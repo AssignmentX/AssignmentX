d51f53683a71c90d6ce4ab44c2ce16f07a1cfbed
Zach <zes15@my.fsu.edu>
2018-03-29 14:01:50 -0400

    adds checkered board to chessboard

--- ChessBoardPanel.java
+++ ChessBoardPanel.java
@@ -4,6 +4,11 @@ import javax.swing.JPanel;
 
 @SuppressWarnings("serial") // this is used to suppress a serializable warning because JPanel implements serializable
 public class ChessBoardPanel extends JPanel {
+    //public ChessBoardPanel() {
+    //    super(new GridLayout(4, 4, 5, 5));
+    //    panel.setBackground(Color.BLACK);
+    //}
+
     public void paintComponent( Graphics g ) {
         super.paintComponent( g ); // call superclass's paintComponent
         Graphics2D g2d = ( Graphics2D ) g;
