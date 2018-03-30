e348cb6130eae77475851c279e2264368d332641
Zach <zes15@my.fsu.edu>
2018-03-30 19:55:33 -0400

    adds chess png images

--- ChessSquarePanel.java
+++ ChessSquarePanel.java
@@ -3,22 +3,30 @@ import java.awt.Graphics2D;
 import java.awt.Color;
 import javax.swing.JPanel;
 import javax.swing.BorderFactory;
+import javax.swing.ImageIcon;
+
+import javax.swing.JLabel;
 
 
 @SuppressWarnings("serial") // this is used to suppress a serializable warning because JPanel implements serializable
 public class ChessSquarePanel extends JPanel {
     private String piece;
     private String player;
+    private JLabel imgLabel;
+
     public ChessSquarePanel() {
         super();
         setBorder(BorderFactory.createLineBorder(Color.BLACK)); // add black border
-        //piece = new String("");
-        //player = new String("");
     }
 
     public void setPiece(String piece, String player) {
         this.piece = piece;
         this.player = player;
+
+        imgLabel = new JLabel(new ImageIcon("images/reactor.png"));
+        imgLabel.setLocation(5, 5);
+        add(imgLabel);
+        repaint();
     }
 
     public String getPiece() {
