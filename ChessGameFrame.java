f23c4bb3124de3ba7b9c4a1d6a5b962d8fc09024
Zach <zes15@my.fsu.edu>
2018-03-31 17:42:05 -0400

    adds textArea to EAST side of frame

--- ChessGameFrame.java
+++ ChessGameFrame.java
@@ -6,61 +6,34 @@ import java.awt.Color;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JButton;
+import javax.swing.JTextArea;
 import javax.swing.BorderFactory;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 
 
 @SuppressWarnings("serial") // this is used to suppress a serializable warning because JFrame implements serializable
-public class ChessGameFrame extends JFrame implements ActionListener 
-{
+public class ChessGameFrame extends JFrame {
    private JButton buttons[]; // array of buttons to hide portions
-   //private JPanel squares[];
+   private JTextArea textArea;
    private ChessBoardPanel chessBoard;
-   private final String names[] = { "North", "South", 
-      "East", "West"};
+   private final String names[] = { "North", "South", "West"};
    private BorderLayout layout; // borderlayout object
 
    // set up GUI and event handling
    public ChessGameFrame(String s)
    {
-      super(s);
+      super(s); // pass title for window to parent constructor
 
       layout = new BorderLayout( 5, 5 ); // 5 pixel gaps
       setLayout( layout ); // set frame layout
 
-      // these buttons are placeholders
-      buttons = new JButton[ names.length ]; // set size of array
+      textArea = new JTextArea(0, 10);
+      textArea.setBorder(BorderFactory.createLineBorder(Color.BLACK)); // add black border
+      add(textArea, BorderLayout.EAST); // add text area to east side
 
       chessBoard = new ChessBoardPanel(); // create a panel for the chess board
-      add( chessBoard, BorderLayout.CENTER ); // add chess board to center
-
-      // buttons are being used as placeholders
-      for ( int count = 0; count < names.length; count++ ) 
-      {
-         buttons[ count ] = new JButton( names[ count ] );
-         buttons[ count ].addActionListener( this );
-      }
-
-      add( buttons[ 0 ], BorderLayout.NORTH ); // add button to north
-      add( buttons[ 1 ], BorderLayout.SOUTH ); // add button to south
-      add( buttons[ 2 ], BorderLayout.EAST ); // add button to east
-      add( buttons[ 3 ], BorderLayout.WEST ); // add button to west
+      add(chessBoard, BorderLayout.CENTER); // add chess board to center
       
-   }
-
-   // handle button events
-   public void actionPerformed( ActionEvent event ) {
-   //{
-   //   // check event source and lay out content pane correspondingly
-   //   for ( JButton button : buttons )
-   //   {
-   //      if ( event.getSource() == button )
-   //         button.setVisible( false ); // hide button clicked
-   //      else
-   //         button.setVisible( true ); // show other buttons
-   //   } // end for
-
-      layout.layoutContainer( getContentPane() ); // lay out content pane
    }
 }
